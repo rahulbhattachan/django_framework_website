c7325a698c7d10858bb96c20a33599338c424504
rahulbhattachan <123595115+rahulbhattachan@users.noreply.github.com>
2024-04-22 06:50:59 -0500

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -153,7 +153,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rahulab590@gmail.com'
-EMAIL_HOST_PASSWORD = 'Luhar202020999!!'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_TLS_CERTFILE = certifi.where()
 '''
